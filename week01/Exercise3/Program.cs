fe46141f0e2dc5287b957fb4ebe39213073cf030
rodrigo-olivarez <olivarezkronnoz@gmail.com>
2026-03-07 16:20:16 -0600

    edit the 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,20 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("What is the magic number? ");
-        int number = int.Parse(Console.ReadLine());
+        //Console.WriteLine("What is the magic number? ");
+        //int number = int.Parse(Console.ReadLine());
+
+        Random RandomGenerator = new Random();
+        int RandomNumber = RandomGenerator.Next(1, 101);
         int guess = -1;
 
-        while (guess != number)
+        while (guess != RandomNumber)
         {
             Console.WriteLine("What is your guess? ");
             guess = int.Parse(Console.ReadLine());
 
-            if (number > guess)
+            if (RandomNumber > guess)
             {
                 Console.WriteLine("Higher");
             }
-            else if (number < guess)
+            else if (RandomNumber < guess)
             {
                 Console.WriteLine("Lower");
             }
